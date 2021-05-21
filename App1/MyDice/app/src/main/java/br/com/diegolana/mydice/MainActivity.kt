1198d5e1f437d96ca3519403d71b19fe5ef57eb9
Diego Lana <diegok@ciandt.com>
2021-05-21 17:51:02 -0300

    My Dice - Therd Class

--- App1/MyDice/app/src/main/java/br/com/diegolana/mydice/MainActivity.kt
+++ App1/MyDice/app/src/main/java/br/com/diegolana/mydice/MainActivity.kt
@@ -1,5 +1,7 @@
 package br.com.diegolana.mydice
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -12,9 +14,10 @@ import kotlin.random.Random
 class MainActivity : AppCompatActivity() {
 
     private lateinit var image : ImageView
-    private lateinit var text : TextView
+    private lateinit var textView : TextView
     private lateinit var button : Button
     private val listImage = listOf(R.drawable.dice1,R.drawable.dice2,R.drawable.dice3,R.drawable.dice4,R.drawable.dice5,R.drawable.dice6)
+    private var contentText = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,9 +27,10 @@ class MainActivity : AppCompatActivity() {
 
     private fun setup() {
         image = findViewById(R.id.imageView)
-        text = findViewById(R.id.textView)
+        textView = findViewById(R.id.textView)
         button = findViewById(R.id.button)
-        roll()
+        contentText = loadData()
+        textView.text = contentText
 
         button.setOnClickListener{
             roll()
@@ -37,9 +41,27 @@ class MainActivity : AppCompatActivity() {
         val diceResult = Random.nextInt(0,6)
         val timeResult = SimpleDateFormat("HH:mm:ss.SSS").format(Date())
         val lastLine = "${diceResult+1} : $timeResult \n"
-        val oldText = text.text
-        text.text = lastLine + oldText
+        contentText += lastLine
+        textView.text = contentText
 
         image.setImageResource(listImage[diceResult])
+    }
+
+    override fun onStop() {
+        saveData(contentText)
+        super.onStop()
+    }
+
+    private fun saveData(str: String) {
+        val prefs: SharedPreferences = this.getSharedPreferences("shared", Context.MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = prefs.edit()
+        editor.putString("key", str)
+        editor.apply()
+        editor.commit()
+    }
+
+    private fun loadData(): String {
+        val prefs: SharedPreferences = this.getSharedPreferences("shared", Context.MODE_PRIVATE)
+        return prefs.getString("key", "") ?: ""
     }
 }
